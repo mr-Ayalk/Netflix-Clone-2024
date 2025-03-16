2f29013bc21c80125d841653bd9781d2d74579b6
mr-Ayalk <ayalkteketel7@gmail.com>
2025-03-16 18:15:14 +0300

    axios and request

---
+++ src/utils/requests.js
@@ -0,0 +1,25 @@
+const API_KEY =process.env.REACT_APP_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+};
+
+export default requests;
+
+
+
+
+
+
+
+
+
+// https://api.themoviedb.org/3//discover/tv?api_key=808c13325d7544bc83168840e101bda8&with_networks%20213
